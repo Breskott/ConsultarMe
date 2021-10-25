e2d8405a689e3f298c372a358f5efa1a1356d6d4
victorbrescott@hotmail.com <Breskott>
2021-10-24 22:47:04 -0300

    Correção da nomenclatura da tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                 <li><a href="{{ route('home') }}">Início</a></li>
                 <li><a href="{{ route('login') }}">Entrar</a></li>
             </ol>
-            <h2>Login</h2>
+            <h2>Entrar</h2>
         </div>
     </section><!-- End Breadcrumbs -->
 
@@ -24,7 +24,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                        <div class="card-header">{{ __('Login') }}</div>
+                        <div class="card-header">{{ __('Entrar') }}</div>
 
                         <div class="card-body">
                             <form method="POST" action="{{ route('login') }}">
