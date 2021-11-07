10c42bc1d5489a3c8f93d22b18d4f9d6612ee2d8
victorbrescott@hotmail.com <Breskott>
2021-11-07 04:59:36 -0300

    Modelado banco de dados com Workbench

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,13 +18,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // Insere tarumã como padrão e cidades proximas
         DB::table('cities')->insert([
             'description'   => 'Tarumã',
             'state'           => 'SP'
         ]);
+        DB::table('cities')->insert([
+            'description'   => 'Assis',
+            'state'         => 'SP'
+        ]);
+        DB::table('cities')->insert([
+            'description'   => 'Cândido Mota',
+            'state'         => 'SP'
+        ]);
 
+        DB::table('cities')->insert([
+            'description'   => 'Florínea',
+            'state'         => 'SP'
+        ]);
+
+        // Cria o usuário administrador
         DB::table('users')->insert([
-            'name'          => 'Victor Brescott Leandro Figueiredo',
+            'name'          => 'Victor Brescott',
             'email'         => 'victorbrescott@hotmail.com',
             'cpf'           => '387.921.858-78',
             'is_permission' => 2, // Admin
@@ -36,9 +52,20 @@ class DatabaseSeeder extends Seeder
             'complement'    => 'Casa perto da árvore',
             'city_id'       => 1,
             'birth_date'    => '1997-03-03',
-            'phone'         => '189990-0001',
+            'phone'         => '(18) 9990-0001',
             'created_at'    => Carbon::now(),
             'updated_at'    => Carbon::now(),
+        ]);
+
+        // Cria um posto de saúde
+        DB::table('units')->insert([
+            'description'   => 'PSF Lagos/Árvores',
+            'phone'         => ' (18) 3373-4502',
+            'zip_code'      => '19820-000',
+            'address'       => 'Rua Canjarana',
+            'number'        => '73',
+            'distric'       => 'Vila das Árvores',
+            'city_id'       => 1,
         ]);
     }
 }
